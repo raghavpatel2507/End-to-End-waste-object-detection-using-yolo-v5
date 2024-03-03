d7c4d10feaf50cfb8db48c00b657edd291df6dac
raghav patel <patelraghav014@gmail.com>
2024-03-03 19:10:58 +0530

    model_training added

---
+++ wasteDetection/components/model_trainer.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import yaml
+import zipfile
+import shutil
+from wasteDetection.utils.main_utils import read_yaml_file
+from wasteDetection.logger import logging
+from wasteDetection.exception import AppException
+from wasteDetection.entity.config_entity import ModelTrainerConfig
+from wasteDetection.entity.artifacts_entity import ModelTrainerArtifact
+
+class ModelTrainer:
+    def __init__(
+        self,
+        model_trainer_config: ModelTrainerConfig,
+    ):
+        self.model_trainer_config = model_trainer_config
+
+    def initiate_model_trainer(self,) -> ModelTrainerArtifact:
+        logging.info("Entered initiate_model_trainer method of ModelTrainer class")
+
+        try:
+            logging.info("Unzipping data")
+            with zipfile.ZipFile("data.zip", 'r') as zip_ref:
+                zip_ref.extractall(".")
+            
+            os.remove("data.zip")
+            with open("data.yaml", 'r') as stream:
+                num_classes = str(yaml.safe_load(stream)['nc'])
+
+            model_config_file_name = self.model_trainer_config.weight_name.split(".")[0]
+            print(model_config_file_name)
+
+            config = read_yaml_file(f"yolov5/models/{model_config_file_name}.yaml")
+
+            config['nc'] = int(num_classes)
+
+            with open(f'yolov5/models/custom_{model_config_file_name}.yaml', 'w') as f:
+                yaml.dump(config, f)
+
+            os.system(f"cd yolov5/ && python train.py --img 416 --batch {self.model_trainer_config.batch_size} --epochs {self.model_trainer_config.no_epochs} --data ../data.yaml --cfg ./models/custom_yolov5s.yaml --weights {self.model_trainer_config.weight_name} --name yolov5s_results  --cache")
+            shutil.copy("yolov5/runs/train/yolov5s_results/weights/best.pt", "yolov5/")
+            os.makedirs(self.model_trainer_config.model_trainer_dir, exist_ok=True)
+            shutil.copy("yolov5/runs/train/yolov5s_results/weights/best.pt", self.model_trainer_config.model_trainer_dir)
+            
+            shutil.rmtree('yolov5/runs', ignore_errors=True)
+            shutil.rmtree("train", ignore_errors=True)
+            shutil.rmtree("valid", ignore_errors=True)
+            os.remove("data.yaml")
+            
+
+            model_trainer_artifact = ModelTrainerArtifact(
+                trained_model_file_path="yolov5/best.pt",
+            )
+
+            logging.info("Exited initiate_model_trainer method of ModelTrainer class")
+            logging.info(f"Model trainer artifact: {model_trainer_artifact}")
+
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise AppException(e, sys)
--- wasteDetection/constant/training_pipeline/__init__.py
+++ wasteDetection/constant/training_pipeline/__init__.py
@@ -21,13 +21,13 @@ DATA_VALIDATION_STATUS_FILE = 'status.txt'
 DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "valid", "data.yaml"]
 
 
-# """
-# MODEL TRAINER related constant start with MODEL_TRAINER var name
-# """
-# MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+"""
+MODEL TRAINER related constant start with MODEL_TRAINER var name
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
 
-# MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov5s.pt"
+MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov5s.pt"
 
-# MODEL_TRAINER_NO_EPOCHS: int = 1 #i am run the model 5 epochs you can try more ephochs and more eccuracy.
+MODEL_TRAINER_NO_EPOCHS: int = 1 #i am run the model 5 epochs you can try more ephochs and more eccuracy.
 
-# MODEL_TRAINER_BATCH_SIZE: int = 16
+MODEL_TRAINER_BATCH_SIZE: int = 16
